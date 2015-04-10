b1b06f32568bbbbc77bde221021440e1764618a5
Gabriel Ferreira <gabriel.d3v@gmail.com>
2015-04-10 18:47:32 -0300

    Refactory

--- cwlogs.py
+++ cwlogs.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request
+from boto import ec2
+import os
+s_key = os.environ.get('AWS_SECRET_ACCESS_KEY_ID') if os.environ.has_key('AWS_SECRET_ACCESS_KEY_ID') else None
+a_key = os.environ.get('AWS_ACCESS_KEY_ID') if os.environ.has_key('AWS_ACCESS_KEY_ID') else None
+region = os.environ.get('REGION') if os.environ.has_key('REGION') else 'us-east-1'
+
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,10 +18,13 @@ def export():
     region = request.args.get('region')
     daysAgo = request.args.get('d_ago', 0, type=int)
 
-@app.route('/_get_regions'):
-    return ''
+@app.route('/_get_regions', methods=['POST', 'GET'])
+def get_regions():
+    regions = ec2.regions(aws_access_key_id=a_key, aws_secret_access_key=s_key)
+    return jsonify(regions=[r.name for r in regions])
 
-@app.route('/_get_l_groups'):
+@app.route('/_get_l_groups')
+def get_log_groups():
     return ''
 
 if __name__ == '__main__':
